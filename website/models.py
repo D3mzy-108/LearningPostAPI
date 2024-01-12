1bcc0ed97b01080104f8e30e089028784515ea6f
D3mzy-108 <demil.ayinde@gmail.com>
2024-01-12 20:16:44 +0100

    Made minor changes to UI, and added profile_photo to user model

--- website/models.py
+++ website/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.username
