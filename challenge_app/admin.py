1c3d778dc1ae341439800a05ec8b225863d0227e
D3mzy-108 <demil.ayinde@gmail.com>
2024-03-12 11:08:09 +0100

    ...

--- challenge_app/admin.py
+++ challenge_app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import ChallengeRoom
+from .models import ChallengeRoom, ChallengeScore
 
 
 @admin.register(ChallengeRoom)
 class ChallengeRoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ChallengeScore)
+class ChallengeScoreAdmin(admin.ModelAdmin):
     pass
