e8c60b9177d354df3f89ec508f7ddc7ebd41d7ca
D3mzy-108 <demil.ayinde@gmail.com>
2024-03-12 21:07:51 +0100

    created get challenge scores endpoint

--- challenge_app/urls.py
+++ challenge_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_room, delete_room, get_challenge_questions, join_room, save_score
+from .views import create_room, delete_room, get_challenge_questions, get_challenge_scores, join_room, save_score
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('delete/<slug:slug>/', delete_room),
     path('questions/<int:testid>/<int:limit>/', get_challenge_questions),
     path('save-score/', save_score),
+    path('get-challenge-scores/<slug:room_slug>/', get_challenge_scores),
 ]
