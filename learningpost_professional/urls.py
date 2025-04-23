90acd515e40da07ed5fbd3aab89a30e77fff9883
D3mzy-108 <demil.ayinde@gmail.com>
2025-04-23 19:10:14 +0100

    Created professional auth views

--- learningpost_professional/urls.py
+++ learningpost_professional/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from learningpost_professional.views import pro_quests
+from learningpost_professional.views import pro_quests, professional_login, professional_signup
 
 urlpatterns = [
+    path('professional-signup/', professional_signup),
+    path('professional-login/', professional_login),
     path('get-quests/<str:username>/', pro_quests),
 ]
--- learningpost_professional/views.py
+++ learningpost_professional/views.py
@@ -5,6 +5,71 @@ from admin_app.models import Quest
 from endpoints.api_views.quest_views import _build_quest_object
 from learningpost_professional.models import ProfessionalOrganization
 from website.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.contrib import auth
+
+
+@require_POST
+@csrf_exempt
+def professional_signup(request):
+    first_name = request.POST.get('first_name') or ''
+    last_name = request.POST.get('last_name') or ''
+    email = request.POST.get('email')
+    username = request.POST.get('username') or email.split('@')[0]
+    password = request.POST.get('password')
+    company_code = request.POST.get('company_code')
+
+    if not User.objects.filter(username=username).exists():
+        user_instance = User()
+        user_instance.first_name = first_name
+        user_instance.last_name = last_name
+        user_instance.email = email
+        user_instance.username = username
+        user_instance.set_password(password)
+        user_instance.save()
+
+    user = get_object_or_404(User, username=username)
+    organization = get_object_or_404(
+        ProfessionalOrganization, organization_code=company_code)
+    organization.members.add(user)
+    organization.save()
+
+    return JsonResponse({
+        'success': True,
+        'message': 'Account Registered!',
+    })
+
+
+@require_POST
+@csrf_exempt
+def professional_login(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    try:
+        user = User.objects.get(email=email)
+        if user.check_password(password):
+            auth.login(request, user=user)
+            return JsonResponse({
+                'success': True,
+                'message': 'Login successful!',
+                'user': {
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email,
+                    'username': user.username,
+                },
+            })
+        else:
+            return JsonResponse({
+                'success': False,
+                'message': 'Invalid credentials!'
+            })
+    except:
+        return JsonResponse({
+            'success': False,
+            'message': 'Invalid credentials!'
+        })
 
 
 def pro_quests(request, username):
